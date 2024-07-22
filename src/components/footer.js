3292ac7cadbd30c09584272cc400f3c807aa7cdc
JulianGrafen <juliangraefen@gmail.com>
2024-07-22 10:12:54 +0200

    small SEO changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Falls Sie React Router verwenden
+
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-center py-4">
       <h1 className="font-poppins font-semibold text-white text-base">WIE KAUFT MAN BITCOIN?</h1>
-      <a 
-        href="/impressum" 
-        className="text-blue-500 hover:underline block mt-2"
-      >
+      <Link to="/impressum" className="text-lg font-semibold hover:text-gray-400">Home</Link>
         Impressum
-      </a>
     </footer>
   );
 };
